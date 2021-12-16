a125fd164d01594713b9e17aba90f97c6d128a0b
julian1 <mail@julian1.io>
2021-12-16 11:42:27 +1100

    update

--- main2.cpp
+++ main2.cpp
@@ -58,16 +58,6 @@ bool write_ppm(const unsigned char* buf,
 
 int main()
 {
-    //--------------------
-    // Allocate the buffer.
-    // Clear the buffer, for now "manually"
-    // Create the rendering buffer object
-    // Create the Pixel Format renderer
-    // Create one line (span) of type rgba8.
-    // Fill the buffer using blend_color_span
-    // Write the buffer to agg_test.ppm
-    // Free memory
-
     unsigned char* buffer = new unsigned char[frame_width * frame_height * 3];
 
     memset(buffer, 255, frame_width * frame_height * 3);
@@ -77,37 +67,17 @@ int main()
                                frame_height, 
                                frame_width * 3);
 
-    agg::pixfmt_rgb24 pixf(rbuf);
 
 
+    // typedef agg::pixfmt_rgb565 pixfmt;
+    typedef agg::pixfmt_rgb24  pixfmt_t;
 
-  
-    // typedef  renderer_base;
+    pixfmt_t  pixf(rbuf);
 
-    //typedef agg::pixfmt_rgb565 pixfmt;
-    // pixfmt pixf(rbuf);
 
-    agg::renderer_base<agg::pixfmt_rgb24>   rb(pixf);
-
+     agg::renderer_base<pixfmt_t>   rb(pixf);
 
      rb.clear(agg::rgba(1,0,0));     // green.
-
-#if 0
-    agg::rgba8 span[frame_width];
-
-    unsigned i;
-    for(i = 0; i < frame_width; ++i)
-    {
-        agg::rgba c(380.0 + 400.0 * i / frame_width, 0.8);
-        span[i] = agg::rgba8(c);
-    }
-
-
-    for(i = 0; i < frame_height; ++i)
-    {
-        pixf.blend_color_hspan(0, i, frame_width, span, 0, 0);
-    }
-#endif
 
 
       unsigned i;
